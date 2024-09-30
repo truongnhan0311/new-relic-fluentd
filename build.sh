f08314ce6aa8dae6a5f255c2408a2d59d61bee28
Nhan.Ng <nguyen.truong.nhan@uleeco.com>
2024-09-30 10:58:33 +0700

    add build es

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+docker build -f Dockerfile-es -t="truongnhan0311/ilogis:newrelic-fluentd-docker-es" .
 docker build --no-cache -t="truongnhan0311/ilogis:newrelic-fluentd-docker" .
+
